3b4533b2b450f9329bf482b346a44cb484eb31b0
shallow90125 <shallow90125@gmail.com>
2023-11-07 11:41:38 +0900

    fix bug

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -2,6 +2,8 @@ import { PiDoc } from "@/types/PiDoc";
 import { MongoClient } from "mongodb";
 import { config } from "./config";
 
-export const db = new MongoClient(config.mongo.url).db(config.mongo.db);
+export const db = new MongoClient(config.mongo.url, {
+  auth: { username: config.mongo.username, password: config.mongo.password },
+}).db(config.mongo.db);
 
 export const piCol = db.collection<PiDoc>("pi");
